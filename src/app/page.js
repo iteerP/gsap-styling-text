6d7640a79adeecd4e9c8f53d5191363040127eee
iteerP <preetigobindpuri02@gmail.com>
2024-05-08 21:47:13 -0700

    done?

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,18 @@
-'use client'
+'use client';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { useEffect, useState, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
   const [words, setWords] = useState([]);
-  const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.";
+  const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.";
 
-  function wordToChars(word) {
-    const charArray = [];
-    const n = word.length;
-    for(let i = 0; i < n; ++i)
-    {
-      charArray.push(word[i]);
-    }
-    return charArray;
+  let animation;
+  const playAnim = () => {
+    animation.play();
   }
+  
+  const audio = new Audio('/beep.mp3');
 
   useEffect(() => {
     const wordArray = text.split(' ');
@@ -23,27 +20,36 @@ export default function Home() {
     setWords(wordsArray);
   }, []);
 
+  function wordToChars(word) {
+    const charArray = [];
+    const n = word.length;
+    for (let i = 0; i < n; ++i) {
+      charArray.push(word[i]);
+    }
+    return charArray;
+  }
+
   useGSAP(() => {
-    console.log("Animating elements:", document.querySelectorAll('.js-animateText'));
-    gsap.from('.js-animateText', {
+    const animatedElements = gsap.utils.toArray('.js-animateText');
+    animation = gsap.from(animatedElements, {
       duration: 0.05,
-      stagger: 0.045,
+      stagger: {
+        each: 0.045,
+        onStart: () => {
+          audio.currentTime = 0;
+          audio.play();
+        }
+      },
       opacity: 0,
-      onStart: () => {
-        console.log('iteration');
-      }
     });
+    animation.pause();
   }, [words]);
 
-  const playSound = () => {
-    const sound = document.getElementById('audio');
-    sound.play();
-    console.log('clicked');
-  }
   
+
   return (
-    <main className="h-[100vh] w-auto bg-white grid place-content-center overflow-hidden">
-      <div className='text-black mx-[10vw]'>
+    <main className="h-screen w-full bg-white flex flex-col justify-center items-center overflow-hidden">
+      <div className='text-black mx-[10vw] w-[80vw] h-[30vh] text-center overflow-scroll scroll-smooth'>
         {words.map((word, idx1) => (
           <div className='inline-block' key={idx1}>
             {word.map((char, idx2) => (
@@ -52,13 +58,9 @@ export default function Home() {
             <span className='text-7xl'>{'\u00A0'}</span>
           </div>
         ))}
-
-        <button onClick={playSound}>Click me</button>
-
-        <audio id='audio'>
-          <source src='https://drive.google.com/file/d/119n8FezrZ_fT6Nr0IlMEXifV3_8pDAj4/view' />
-        </audio>
       </div>
+
+      <button onClick={() => {playAnim(); console.log('button clicked')}}>Click to Play</button>
     </main>
   );
 }
